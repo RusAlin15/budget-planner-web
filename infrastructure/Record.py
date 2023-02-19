c13f9d8823b3875bde114dcaa6addfa3453b53ff
Alin Rus El-Hassaina <alin.rus.el-hassaina@continental-corporation.com>
2023-02-19 22:02:55 +0200

    Add date parameter to Record

--- infrastructure/Record.py
+++ infrastructure/Record.py
@@ -1,11 +1,15 @@
+from datetime import date
+
+
 class Record:
-    def __init__(self, title, value, description=""):
+    def __init__(self, title, value, date: date, description=""):
         self.__title = title
         self.__description = description
         self.__value = value
+        self.__date = date
 
     def __str__(self):
-        return f"Title: {self.__title} ; Value: {self.__value} ; Description: {self.__description}"
+        return f"Title: {self.__title} ; Value: {self.__value} ; Date: {self.__date} ; Description: {self.__description}"
 
     def __repr__(self):
         return f"{self.__title};{self.__description};{self.__value}"
